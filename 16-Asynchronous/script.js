bf26f34007a3edc5cf5365dbead4bcfc8fbe18b0
Alazy99 <57822868+Alazy99@users.noreply.github.com>
2023-02-01 16:24:18 -0500

    Finshed challenge #3. Did anime API call.

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -481,6 +481,9 @@ createImage2('img/img-1.jpg')
   .catch(err => console.error(err));
 */
 
+/////////////////////////////////////////////////////
+//
+/*
 const getPosition = function () {
   return new Promise(function (resolve, reject) {
     navigator.geolocation.getCurrentPosition(resolve, reject);
@@ -526,10 +529,21 @@ const whereAmI = async function () {
 console.log('1: Will get location');
 // const city = whereAmI();
 // console.log(city);
-whereAmI()
-  .then(city => console.log(`2: ${city}`))
-  .catch(err => console.error(`2: ${err.message}🚨`))
-  .finally(() => console.log('3: Finished getting location'));
+// whereAmI()
+//   .then(city => console.log(`2: ${city}`))
+//   .catch(err => console.error(`2: ${err.message}🚨`))
+//   .finally(() => console.log('3: Finished getting location'));
+
+(async function () {
+  try {
+    const city = await whereAmI();
+    console.log(`2: ${city}`);
+  } catch (err) {
+    console.error(`2: ${err.message}🚨`);
+  }
+  console.log('3: Finished getting location');
+})();
+*/
 
 // try {
 //   let y = 1;
@@ -551,3 +565,175 @@ btn.addEventListener('click', function () {
 nekoBtn.addEventListener('click', function () {
   getWaifuData('sfw', 'neko');
 });
+
+/////////////////////////////////////////////////
+// Running Promises in Parallel
+/*
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    // const [data1] = await getJSON(`https://restcountries.com/v2/name/${c1}`);
+    // const [data2] = await getJSON(`https://restcountries.com/v2/name/${c2}`);
+    // const [data3] = await getJSON(`https://restcountries.com/v2/name/${c3}`);
+    // console.log([data1.capital, data2.capital, data3.capital]);
+
+    const data = await Promise.all([
+      getJSON(`https://restcountries.com/v2/name/${c1}`),
+      getJSON(`https://restcountries.com/v2/name/${c2}`),
+      getJSON(`https://restcountries.com/v2/name/${c3}`),
+    ]);
+    console.log(data.map(d => d[0].capital));
+  } catch (err) {
+    console.error(err);
+  }
+};
+get3Countries('portugal', 'canada', 'tanzania');
+*/
+
+//////////////////////////////////////////////
+// Other Promise Combinators: race, allSettled and any
+/*
+// Promise.race
+(async function () {
+  const res = await Promise.race([
+    getJSON(`https://restcountries.com/v2/name/italy`),
+    getJSON(`https://restcountries.com/v2/name/egypt`),
+    getJSON(`https://restcountries.com/v2/name/mexico`),
+  ]);
+  console.log(res[0]);
+})();
+
+const timeout = function (ms) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error('request took too long!'));
+    }, ms * 1000);
+  });
+};
+
+Promise.race([getJSON(`https://restcountries.com/v2/name/mexico`), timeout(5)])
+  .then(res => console.log(res[0]))
+  .catch(err => console.error(err));
+
+// Promise.allSettled
+Promise.allSettled([
+  Promise.resolve('Success'),
+  Promise.reject('ERROR'),
+  Promise.resolve('Another success'),
+]).then(res => console.log(res));
+
+Promise.all([
+  Promise.resolve('Success'),
+  Promise.reject('ERROR'),
+  Promise.resolve('Another success'),
+])
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
+
+// Promise.any [ES2021]
+Promise.any([
+  Promise.resolve('Success'),
+  // Promise.reject('ERROR'),
+  Promise.resolve('Another success'),
+])
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
+*/
+
+///////////////////////////////////////////////
+// Coding Challenge #3
+/*
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+const imgContainer = document.querySelector('.images');
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', function () {
+      reject(new Error('Image not found'));
+    });
+  });
+};
+
+let currentImg;
+
+// createImage('img/img-1.jpg')
+//   .then(img => {
+//     currentImg = img;
+//     console.log('Image 1 loaded');
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImg.style.display = 'none';
+//     return createImage('img/img-2.jpg');
+//   })
+//   .then(img => {
+//     currentImg = img;
+//     console.log('Image 2 loaded');
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImg.style.display = 'none';
+//     return createImage('img/img-3.jpg');
+//   })
+//   .then(img => {
+//     currentImg = img;
+//     console.log('Image 3 loaded');
+//     return wait(2);
+//   })
+//   .then(() => (currentImg.style.display = 'none'))
+//   .catch(err => console.error(err));
+
+// const loadNPause = async function () {
+//   try {
+//     const img1 = await createImage('img/img-1.jpg');
+//     currentImg = img1;
+//     console.log('Image 1 Loaded');
+//     await wait(2);
+
+//     currentImg.style.display = 'none';
+//     const img2 = await createImage('img/img-2.jpg');
+//     currentImg = img2;
+//     console.log('Image 2 Loaded');
+//     await wait(2);
+
+//     currentImg.style.display = 'none';
+//     const img3 = await createImage('img/img-3.jpg');
+//     currentImg = img3;
+//     console.log('Image 3 Loaded');
+//     await wait(2);
+
+//     currentImg.style.display = 'none';
+//   } catch (err) {
+//     img.addEventListener('error', function () {
+//       console.error(err);
+//     });
+//   }
+// };
+
+// loadNPause();
+
+const loadAll = async function (imgArr) {
+  try {
+    const imgs = imgArr.map(async imgs => await createImage(imgs));
+    const imgEl = await Promise.all(imgs);
+    imgEl.forEach(x => x.classList.add('parallel'));
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// loadAll(['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']);
+loadAll(['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']);
+*/
