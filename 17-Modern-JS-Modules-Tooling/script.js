56b0246ad65398f65d3cc96030a2c8032305b500
Alazy99 <57822868+Alazy99@users.noreply.github.com>
2023-02-02 15:50:18 -0500

    Played with modules

--- 17-Modern-JS-Modules-Tooling/script.js
+++ 17-Modern-JS-Modules-Tooling/script.js
@@ -1,3 +1,31 @@
 // Importing module
-import './shoppingCart.js';
+// import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+
+// addToCart('bread', 5);
+// console.log(price, tq);
+
 console.log('Importing module');
+//console.log(shippingCost);
+
+// import * as ShoppingCart from './shoppingCart.js';
+// ShoppingCart.addToCart('bread', 5);
+// console.log(ShoppingCart.totalPrice);
+
+// import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+// console.log(price);
+
+// import add, { cart } from './shoppingCart.js';
+// add('burgers', 2);
+// add('bread', 5);
+// add('pizza', 2);
+// console.log(cart);
+
+import {
+  championStatus,
+  registerWrestler,
+  superstars,
+} from './shoppingCart.js';
+registerWrestler('Roman Reigns', 'World Wrestling Entertainment (WWE)');
+registerWrestler('MJF', 'All Elite Wrestling (AEW)');
+championStatus();
+console.log(superstars);
--- 17-Modern-JS-Modules-Tooling/shoppingCart.js
+++ 17-Modern-JS-Modules-Tooling/shoppingCart.js
@@ -1,2 +1,42 @@
 // Exporting module
 console.log('Exporting module');
+
+const shippingCost = 10;
+export const cart = [];
+
+export const addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart!`);
+};
+
+const totalPrice = 237;
+const totalQuantity = 23;
+
+export { totalPrice, totalQuantity as tq };
+
+// export default function (product, quantity) {
+//   cart.push({ product, quantity });
+//   console.log(`${quantity} ${product} added to cart!`);
+// }
+
+export const superstars = [];
+
+export const registerWrestler = function (superstar, company) {
+  superstars.push({ superstar, company });
+  console.log(`${superstar} is currently contracted to ${company}`);
+};
+
+export const championStatus = function () {
+  const wweChamp = superstars.find(
+    wrestler => wrestler.superstar === 'Roman Reigns'
+  );
+  const aewChamp = superstars.find(wrestler => wrestler.superstar === 'MJF');
+  if (wweChamp) {
+    console.log(
+      `${wweChamp.superstar} is the current WWE Undisputed Universal Champion`
+    );
+  }
+  if (aewChamp) {
+    console.log(`${aewChamp.superstar} is the current AEW Champion`);
+  }
+};
