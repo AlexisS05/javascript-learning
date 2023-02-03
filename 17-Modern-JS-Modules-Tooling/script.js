58aa88ef623d25fb1e79a44ea045a6c7e48a9eff
Alazy99 <57822868+Alazy99@users.noreply.github.com>
2023-02-02 21:25:12 -0500

    Polyfilling, Babel, npm packages.

--- 17-Modern-JS-Modules-Tooling/script.js
+++ 17-Modern-JS-Modules-Tooling/script.js
@@ -4,7 +4,7 @@
 // addToCart('bread', 5);
 // console.log(price, tq);
 
-console.log('Importing module');
+// console.log('Importing module');
 //console.log(shippingCost);
 
 // import * as ShoppingCart from './shoppingCart.js';
@@ -20,6 +20,9 @@ console.log('Importing module');
 // add('pizza', 2);
 // console.log(cart);
 
+//////////////////////////////////////////
+// PLaying with imports
+
 import {
   championStatus,
   registerWrestler,
@@ -29,3 +32,125 @@ registerWrestler('Roman Reigns', 'World Wrestling Entertainment (WWE)');
 registerWrestler('MJF', 'All Elite Wrestling (AEW)');
 championStatus();
 console.log(superstars);
+
+//////////////////////////////////////////////////////
+// Top-Level await(ES2022)
+/*
+// console.log('Start fetching');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+// console.log('Something');
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = getLastPost();
+console.log(lastPost);
+
+// Get the return but not very clean
+// lastPost.then(last => console.log(last));
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
+*/
+
+//////////////////////////////////////////////////////
+// The Module Pattern
+/*
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart! (shipping cost it ${shippingCost})`
+    );
+  };
+
+  const orderStock = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+// Closures basically. I realized it pretty quickly.
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost);
+*/
+
+////////////////////////////////////////////////
+// NODE JS
+// Export
+/*
+export.addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart! (shipping cost it ${shippingCost})`
+    );
+};
+  
+// Import
+const { addToCart } = require('./shoppingCart.js');
+*/
+
+// import cloneDeep from './node_modules/lodash-es/cloneDeep.js ';
+import cloneDeep from 'lodash-es';
+
+const state = {
+  cart: [
+    {
+      product: 'bread',
+      quantity: 5,
+    },
+    {
+      product: 'pizza',
+      quantity: 5,
+    },
+  ],
+  user: { loggedIn: true },
+};
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+
+state.user.loggedIn = false;
+console.log(stateClone);
+
+console.log(stateDeepClone);
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+class Person {
+  #greeting = 'Hey';
+  constructor(name) {
+    this.name = name;
+    console.log(`${this.#greeting}, ${this.name}`);
+  }
+}
+const alazy = new Person('Alazy');
+
+console.log('Alazy' ?? null);
+
+console.log(superstars.find(el => el.superstar === 'Roman Reigns'));
+Promise.resolve('TEST').then(x => console.log(x));
+
+import 'core-js/stable';
+
+// Polifilling async function
+import 'regenerator-runtime/runtime';
