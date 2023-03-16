a39a03a7bd0aaa1c739476705d66540e37731f44
Alazy99 <57822868+Alazy99@users.noreply.github.com>
2023-03-16 19:31:10 -0400

    learned to implement bookmarks and refactor data

--- 18-forkify/src/js/views/resultsView.js
+++ 18-forkify/src/js/views/resultsView.js
@@ -1,4 +1,5 @@
 import icons from 'url:../../img/icons.svg';
+import previewView from './previewView.js';
 
 import View from './View.js';
 
@@ -9,23 +10,7 @@ class ResultsView extends View {
 
   _generateMarkup() {
     console.log(this._data);
-    return this._data.map(this._generateMarkupPreview).join('');
-  }
-
-  _generateMarkupPreview(result) {
-    return `
-      <li class="preview">
-        <a class="preview__link " href="#${result.id}">
-          <figure class="preview__fig">
-            <img src="${result.image}" alt="${result.title}" />
-          </figure>
-          <div class="preview__data">
-            <h4 class="preview__title">${result.title}</h4>
-            <p class="preview__publisher">${result.publisher}</p>
-          </div>
-        </a>
-      </li>
-      `;
+    return this._data.map(result => previewView.render(result, false)).join('');
   }
 }
 export default new ResultsView();
