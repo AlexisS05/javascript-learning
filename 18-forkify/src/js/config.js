93890eea71363b18b250fa796894cfcd6b5d7f80
Alazy99 <57822868+Alazy99@users.noreply.github.com>
2023-03-18 19:50:54 -0400

    uploading recipe part 1

--- 18-forkify/src/js/config.js
+++ 18-forkify/src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = '8bcc19b0-0e21-431f-96b6-29bc35108b1d';
+export const MODAL_CLOSE_SEC = 2.5;
